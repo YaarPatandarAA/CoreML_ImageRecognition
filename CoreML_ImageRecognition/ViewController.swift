7e723b821ea27cd2fb3ce6873dd8d32eab71e673
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-06 19:54:03 -0800

    Initial Commit

---
+++ CoreML_ImageRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML_ImageRecognition
+//
+//  Created by Amarjit Singh on 12/6/18.
+//  Copyright © 2018 Amarjit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
